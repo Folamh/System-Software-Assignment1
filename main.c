ab0d9ed173daa622b41aa58557d3a07481b910c5
rmurphy <ross.murphy912@gmail.com>
2018-03-14 21:59:14 +0000

    Changed tabs to spaces.

--- main.c
+++ main.c
@@ -4,14 +4,13 @@
 #include "is_time.h"
 
 int main() {
-	openlog("Assignment1-Log", LOG_PID|LOG_CONS, LOG_DAEMON);
-	printf("Before");
-	daemonize();
-	
-	if (is_time(23, 59, 59)) {
-		printf("Hello");
-	}
-	printf("After");
-	closelog();
-	return 0;
+    openlog("Assignment1-Log", LOG_PID|LOG_CONS, LOG_DAEMON);
+    printf("Before");
+    daemonize();
+    if (is_time(23, 59, 59)) {
+        printf("Hello");
+    }
+    printf("After");
+    closelog();
+    return 0;
 }
