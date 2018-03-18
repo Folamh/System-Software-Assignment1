243111ef90a80e2a26534907cc20311f0c9734a9
rmurphy <ross.murphy912@gmail.com>
2018-03-18 13:07:18 +0000

    Better string calls

--- backup.h
+++ backup.h
@@ -22,11 +22,12 @@ void backup(char folder[100], char bak_loc[160]) {
 
     strftime(filename, sizeof(filename), "backup_%Y-%m-%d_%H:%M:%S.tar", timenow);
 
-    char command[300] = "tar czf ";
-    strcat(command, bak_loc);
-    strcat(command, filename);
-    strcat(command, " ");
-    strcat(command, folder);
+    char command[300] = "tar czf";
+//    strcat(command, bak_loc);
+//    strcat(command, filename);
+//    strcat(command, " ");
+//    strcat(command, folder);
+    snprintf(command, sizeof(command), " %s/%s %s", bak_loc, filename, folder);
     syslog(LOG_DEBUG, "Backup command: %s", command);
 
     syslog(LOG_INFO, "Locking down folder for backup.");
