92f6d7fc94143102b5d14d92c8d5d1fd1650ea5b
rmurphy <ross.murphy912@gmail.com>
2018-03-18 21:57:58 +0000

    Time error fixing

--- main.c
+++ main.c
@@ -10,14 +10,19 @@ int main() {
     char report_loc[] = "/var/reports";
     char key[] = "SystemSoftware-Assignment1";
 
+    openlog(key, LOG_PID|LOG_CONS, LOG_DAEMON);
+
     int msqid = msgget((key_t)56565, 0666 | IPC_CREAT);
+    if (msqid < 0) {
+        syslog(LOG_ERR, "Unable to build queue.");
+        closelog();
+        exit(EXIT_FAILURE);
+    }
     struct message {
         long type;
         char text[120];
     } msg;
     long msgtyp = 0;
-
-    openlog(key, LOG_PID|LOG_CONS, LOG_DAEMON);
 
     daemonize();
 
@@ -39,16 +44,17 @@ int main() {
             }
             sleep(1);
         }
-        msgrcv(msqid, (void *) &msg, sizeof(msg.text), msgtyp, MSG_NOERROR | IPC_NOWAIT);
-        if (strcmp(msg.text, "BACKUP") == 0) {
-            syslog(LOG_INFO, "Forcing backup and transfer.");
-            int pid = fork();
-            if (pid == 0) {
-                syslog(LOG_DEBUG, "Forking off backup and transfer.");
-                backup_and_transfer(intranet, bak_loc, live);
+        if (msgrcv(msqid, (void *) &msg, sizeof(msg.text), msgtyp, MSG_NOERROR | IPC_NOWAIT) > 0)  {
+            if (strcmp(msg.text, "BACKUP") == 0) {
+                syslog(LOG_INFO, "Forcing backup and transfer.");
+                int pid = fork();
+                if (pid == 0) {
+                    syslog(LOG_DEBUG, "Forking off backup and transfer.");
+                    backup_and_transfer(intranet, bak_loc, live);
+                }
+            } else {
+                syslog(LOG_INFO, msg.text);
             }
-        } else {
-            //syslog(LOG_INFO, msg.text);
         }
         break;
     }
